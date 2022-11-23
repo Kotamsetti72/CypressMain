f9ff5c29e8b5ac9771b0d22abc8a7f87723874b4
Jatin Kotamsetti <jatin@Jatins-MacBook-Pro.local>
2022-11-23 16:00:23 +0000

    First commit

---
+++ cypress/e2e/test1.cy.js
@@ -0,0 +1,17 @@
+/// reference types = "Cypress"/>
+
+// it('Google search', function() {
+
+// })
+
+it('google search', ()=> {
+cy.visit('https://google.co.uk')
+})
+
+
+
+// describe('empty spec', () => {
+//     it('passes', () => {
+//       cy.visit('https://example.cypress.io')
+//     })
+//   })
